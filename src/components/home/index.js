7c8411c4ab6de8b4cbccf423f6eb3e9650da6665
Alejandro Linan <jandrolinan@gmail.com>
2020-10-19 12:01:29 -0500

    dont merge

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,12 +3,15 @@ import React from 'react';
 class Home extends React.Component{
     render(){
         return(
-            <section>
-                <div className='home'>
+            <section className='home'>
+                <div>
                     <h1 className="home__title">Better Products, Faster.</h1>
                     <p className="home__tagline">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim eget tortor mi congue malesuada condimentu</p>
-                    <img className="home__wide-image" alt="chicago-wide" src={require("../../images/chicago-wide.jpg")} />
                 </div>
+                <div>
+                    <img className="home__wide-image" alt="chicago-wide" src={require("./images/chicago-wide.jpg")} />
+                </div>
+
             </section>
         )
     }
